e558b1ec28ba6f9777ab9f461d91428870b78f6f
AchizuePriscilla <baeewele27@gmail.com>
2020-12-15 03:16:05 +0100

    Firebase Integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:sca_ui/views/screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -9,6 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false, home: WelcomeScreen(),);
+      debugShowCheckedModeBanner: false,
+      home: WelcomeScreen(),
+    );
   }
 }
--- lib/views/screens/register_screen.dart
+++ lib/views/screens/register_screen.dart
@@ -25,73 +25,72 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 backgroundColor: Color(0xff564569),
                 body: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 14.0),
-                  child: ListView(children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          alignment: Alignment.center,
-                          height: SizeConfig.safeBlockVertical * 20,
-                          child: Text(
-                            'Job\nFinder',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: SizeConfig.safeBlockHorizontal * 11,
-                                fontWeight: FontWeight.bold),
-                          ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        height: SizeConfig.safeBlockVertical * 20,
+                        child: Text(
+                          'Job\nFinder',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: SizeConfig.safeBlockHorizontal * 11,
+                              fontWeight: FontWeight.bold),
                         ),
-                        SizedBox(
-                          height: SizeConfig.safeBlockVertical * 5,
-                        ),
-                        TextField(
-                            keyboardType: TextInputType.emailAddress,
-                            textAlign: TextAlign.center,
-                            onChanged: (value) {
-                              email = value;
-                            },
-                            decoration: ktextFieldDecoration.copyWith(
-                                hintText: 'Enter your Email')),
-                        SizedBox(
-                          height: SizeConfig.safeBlockVertical * 1,
-                        ),
-                        TextField(
-                            obscureText: true,
-                            textAlign: TextAlign.center,
-                            onChanged: (value) {
-                              password = value;
-                            },
-                            decoration: ktextFieldDecoration.copyWith(
-                                hintText: 'Enter your Password')),
-                        SizedBox(
-                          height: SizeConfig.safeBlockVertical * 2.5,
-                        ),
-                        CustomButton(
-                          height: SizeConfig.safeBlockVertical * 12,
-                          text: 'Register',
-                          onPressed: () async {
-                            try {
-                              final newUser =
-                                  await _auth.createUserWithEmailAndPassword(
-                                      email: email, password: password);
-
-                              if (newUser != null) {
-                                Navigator.push(context,
-                                    MaterialPageRoute(builder: (context) {
-                                  return LoginScreen();
-                                }));
-                              }
-                            } catch (e) {
-                              print(e);
-                            }
+                      ),
+                      SizedBox(
+                        height: SizeConfig.safeBlockVertical * 5,
+                      ),
+                      TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          textAlign: TextAlign.center,
+                          onChanged: (value) {
+                            email = value;
                           },
-                          buttonColor: Colors.deepPurple[300],
-                          textColor: Colors.white,
-                        ),
-                      ],
-                    ),
-                  ]),
+                          decoration: ktextFieldDecoration.copyWith(
+                              hintText: 'Enter your Email')),
+                      SizedBox(
+                        height: SizeConfig.safeBlockVertical * 1,
+                      ),
+                      TextField(
+                          obscureText: true,
+                          textAlign: TextAlign.center,
+                          onChanged: (value) {
+                            password = value;
+                          },
+                          decoration: ktextFieldDecoration.copyWith(
+                              hintText: 'Enter your Password')),
+                      SizedBox(
+                        height: SizeConfig.safeBlockVertical * 2.5,
+                      ),
+                      CustomButton(
+                        height: SizeConfig.safeBlockVertical * 12,
+                        text: 'Register',
+                        onPressed: () async {
+                          try {
+                            final newUser =
+                                await _auth.createUserWithEmailAndPassword(
+                                    email: email, password: password);
+                            setState(() {
+                              CircularProgressIndicator();
+                            });
+                            if (newUser != null) {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (context) {
+                                return LoginScreen();
+                              }));
+                            }
+                          } catch (e) {
+                            print(e);
+                          }
+                        },
+                        buttonColor: Colors.deepPurple[300],
+                        textColor: Colors.white,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
